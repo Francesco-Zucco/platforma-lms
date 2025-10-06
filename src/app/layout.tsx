d90c0d136d67e7c9fe4cb47d217a4f42e80bbef3
OvidiuAndrei98 <andrei.penica@gmail.com>
2025-10-06 13:31:32 +0300

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <div>salut</div>
       </body>
     </html>
   );
